629d745608ce762d0f01b279bc88ce24b7b5c77c
ankitb21 <bhardwaj21ankit@gmail.com>
2022-02-20 14:29:22 +0530

    Initial Commit

---
+++ Prime Numbers/main.cpp
@@ -0,0 +1,10 @@
+// https://www.geeksforgeeks.org/prime-numbers/?ref=lbp
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  cout << "hello";
+  return 0;
+  return 5;
+  // A prime number is a natural number greater than 1
+}
